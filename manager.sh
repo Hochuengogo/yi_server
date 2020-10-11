59ac859e1c90cfbab14e47cd0647c54c6223ac3f
jiaoyinyi <zaoyihuang@gmail.com>
2020-10-11 23:47:59 +0800

    处理脚本

--- manager.sh
+++ manager.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+# 这个脚本文件要在项目的根路径中
+# 项目根路径
 ROOT=`dirname $0`
+
+# 配置文件
+CFG_FILE="./setting.config"
 
 # 输出函数变量
 PRINT=printf
 
 # rebar变量
 REBAR=rebar3
+
+# erlang变量
+ERL=erl
 
 # 获取依赖库
 function get_dep() {
@@ -18,10 +26,10 @@ function get_dep() {
 # 删除依赖库
 function del_dep() {
     ${PRINT} "开始删除依赖库\n"
-    if [[ -e _build ]] && [[ -d _build ]]; then
+    if [[ -e _build ]]; then
         rm -rf _build
     fi
-    if [[ -e tbin ]] && [[ -d tbin ]]; then
+    if [[ -e tbin ]]; then
         rm -rf tbin
     fi
     ${PRINT} "删除依赖库完成\n"
@@ -30,7 +38,7 @@ function del_dep() {
 # 编译依赖，并将beam复制到tbin目录
 function make_dep() {
     ${PRINT} "开始编译依赖库\n"
-    lib_path=_build/default/lib
+    lib_path="_build/default/lib"
     if [[ -d ${lib_path} ]]; then
         for lib in `find ${lib_path} -type d -maxdepth 1` ; do
             if [[ ${lib} != ${lib_path} ]]; then
@@ -48,7 +56,7 @@ function make_dep() {
 # 复制依赖库的beam、app文件
 function cp_dep() {
     ${PRINT} "开始复制依赖库\n"
-    lib_path=_build/default/lib
+    lib_path="_build/default/lib"
     if [[ ! -e tbin ]]; then
         mkdir tbin
     fi
@@ -56,6 +64,65 @@ function cp_dep() {
         cp -a ${ROOT}/${ebin}/. tbin
     done
     ${PRINT} "复制依赖库完成\n"
+}
+
+# 安装服务器
+function install() {
+    todo
+}
+
+# 卸载服务器
+function uninstall() {
+    todo
+}
+
+# 编译
+function make() {
+    ${PRINT} "开始编译\n"
+    if [[ ! -e ebin ]]; then
+        mkdir ebin
+    fi
+    mods=`find src -type d | sed "s/^/'/;s/$/\/\*'\,/;$ s/.$//" | tr '\n' ' '`
+    opts="[debug_info, {i, \"include\"}, {outdir, \"ebin\"}]"
+    emakefile="[{[${mods}], ${opts}}]"
+    ${ERL} -noshell -eval "make:all([{emake,${emakefile}}])." -s init stop
+    ${PRINT} "编译完成\n"
+}
+
+# 编译模块
+function make_mod() {
+    ${PRINT} "未实现编译模块功能"
+}
+
+# 编译文件
+function make_file() {
+    ${PRINT} "未实现编译文件功能"
+}
+
+# 热更
+function update() {
+    ${PRINT} "未实现热更功能"
+}
+
+# 启动服务器
+function start() {
+    ulimit -S -n 10240 && ${ERL} -pa ebin -pa tbin -s manager start
+}
+
+# 关闭服务器
+function stop() {
+    ${PRINT} "未实现关闭服务器功能"
+}
+
+# 生成日志等级文件
+function gen_log() {
+    log_line=`grep ^\{log_level ${CFG_FILE}`
+    python gen_log.py "${log_line}"
+    if [ $? == 0 ]; then
+        echo "生成日志等级文件成功"
+    else
+        echo "生成日志等级文件失败"
+    fi
 }
 
 case $1 in
@@ -70,5 +137,32 @@ case $1 in
     ;;
   cp_dep)
       cp_dep
+    ;;
+  install)
+      install
+    ;;
+  uninstall)
+      uninstall
+    ;;
+  make)
+      make
+    ;;
+  make_mod)
+      make_mod
+    ;;
+  make_file)
+      make_file
+    ;;
+  update)
+      update
+    ;;
+  start)
+      start
+    ;;
+  stop)
+      stop
+    ;;
+  gen_log)
+      gen_log
     ;;
 esac
---
+++ yi_server.app
@@ -0,0 +1,15 @@
+{application, yi_server,
+ [{description, "game server"},
+  {vsn, "0.1.0"},
+  {registered, []},
+  {mod, {yi_server_app, []}},
+  {applications,
+   [kernel,
+    stdlib
+   ]},
+  {env,[]},
+  {modules, []},
+
+  {licenses, ["Apache 2.0"]},
+  {links, []}
+ ]}.
