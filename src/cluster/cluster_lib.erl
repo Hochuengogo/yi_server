c12d08fe85817f0c9b1c4430d9b98c114f1e1774
jiaoyinyi <zaoyihuang@gmail.com>
2021-10-10 17:21:34 +0800

    跨服模块

--- src/cluster/cluster_lib.erl
+++ src/cluster/cluster_lib.erl
@@ -22,35 +22,21 @@
 
 -include("common.hrl").
 -include("logs.hrl").
+-include("cluster.hrl").
 
 -define(cluster_call_timeout, 5000).
 
 %% @doc 异步执行方法
--spec cast(center | center_ver | all | all_ver | srv_id() | node(), module(), atom(), list()) -> true | {error, term()}.
+-spec cast(center | all | {srv, srv_id()} | {node, node()}, module(), atom(), list()) -> true | {error, term()}.
 %% 中央服节点执行，并且要与中央服节点版本一致
 cast(center, M, F, A) ->
     case srv_lib:server_type() of
         center ->
-            catch util:apply(M, F, A),
-            true;
+            do_cast(local, M, F, A);
         zone ->
             case is_link() of
                 true ->
-                    rpc:cast(srv_lib:center_node(), M, F, A);
-                _ ->
-                    {error, not_link}
-            end
-    end;
-%% 中央服节点执行
-cast(center_ver, M, F, A) ->
-    case srv_lib:server_type() of
-        center ->
-            catch util:apply(M, F, A),
-            true;
-        zone ->
-            case is_link_ver() of
-                true ->
-                    rpc:cast(srv_lib:center_node(), M, F, A);
+                    do_cast(srv_lib:center_node(), M, F, A);
                 _ ->
                     {error, not_link}
             end
@@ -59,16 +45,8 @@ cast(center_ver, M, F, A) ->
 cast(all, M, F, A) ->
     case srv_lib:server_type() of
         center ->
-            catch [rpc:cast(Node, M, F, A) || {_, Node} <- ets:tab2list(srv_id)],
-            true;
-        zone ->
-            {error, not_center}
-    end;
-%% 所有游戏服节点执行
-cast(all_ver, M, F, A) ->
-    case srv_lib:server_type() of
-        center ->
-            catch [rpc:cast(Node, M, F, A) || {_, Node} <- ets:tab2list(srv_id_ver)],
+            CenterVersion = srv_lib:version(),
+            catch [do_cast(Node, M, F, A) || #srv_info{node = Node, version = Version} <- ets:tab2list(srv_info), CenterVersion =:= Version],
             true;
         zone ->
             {error, not_center}
@@ -77,33 +55,13 @@ cast(all_ver, M, F, A) ->
 cast({srv, SrvId}, M, F, A) ->
     case srv_lib:is_local(SrvId) of
         true ->
-            catch util:apply(M, F, A),
-            true;
+            do_cast(local, M, F, A);
         _ ->
             case is_link() of
                 true ->
                     case catch ets:lookup(srv_id, SrvId) of
                         [{_, Node}] ->
-                            cast(Node, M, F, A);
-                        _ ->
-                            {error, srv_not_link}
-                    end;
-                _ ->
-                    {error, not_link}
-            end
-    end;
-%% 指定游戏服节点执行
-cast({srv_ver, SrvId}, M, F, A) ->
-    case srv_lib:is_local(SrvId) of
-        true ->
-            catch util:apply(M, F, A),
-            true;
-        _ ->
-            case is_link_ver() of
-                true ->
-                    case catch ets:lookup(srv_id_ver, SrvId) of
-                        [{_, Node}] ->
-                            cast(Node, M, F, A);
+                            cast({node, Node}, M, F, A);
                         _ ->
                             {error, srv_not_link}
                     end;
@@ -112,77 +70,106 @@ cast({srv_ver, SrvId}, M, F, A) ->
             end
     end;
 %% 游戏节点执行
-cast(Node, M, F, A) when Node =:= node() ->
-    catch util:apply(M, F, A),
-    true;
-cast(Node, M, F, A) when is_atom(Node) ->
+cast({node, Node}, M, F, A) when Node =:= node() ->
+    do_cast(local, M, F, A);
+cast({node, Node}, M, F, A) ->
     case srv_lib:server_type() of
         center ->
-            rpc:cast(Node, M, F, A);
+            do_cast(Node, M, F, A);
         zone ->
-            cast(center, ?MODULE, cast, [Node, M, F, A]) %% 交给中央服执行
+            cast(center, ?MODULE, cast, [{node, Node}, M, F, A]) %% 交给中央服执行
+    end;
+%% 发送消息到进程
+cast({msg, center, Name}, CastMod, CastFunc, Msg) ->
+    case srv_lib:server_type() of
+        center ->
+            do_cast_msg(local, Name, CastMod, CastFunc, Msg);
+        zone ->
+            case is_link() of
+                true ->
+                    do_cast_msg(srv_lib:center_node(), Name, CastMod, CastFunc, Msg);
+                _ ->
+                    {error, not_link}
+            end
+    end;
+cast({msg, all, Name}, CastMod, CastFunc, Msg) ->
+    case srv_lib:server_type() of
+        center ->
+            CenterVersion = srv_lib:version(),
+            catch [do_cast_msg(Node, Name, CastMod, CastFunc, Msg) || #srv_info{node = Node, version = Version} <- ets:tab2list(srv_info), CenterVersion =:= Version],
+            true;
+        zone ->
+            {error, not_center}
+    end;
+cast({msg, {srv, SrvId}, Name}, CastMod, CastFunc, Msg) ->
+    case srv_lib:is_local(SrvId) of
+        true ->
+            do_cast_msg(local, Name, CastMod, CastFunc, Msg);
+        _ ->
+            case is_link() of
+                true ->
+                    case catch ets:lookup(srv_id, SrvId) of
+                        [{_, Node}] ->
+                            cast({msg, {node, Node}, Name}, CastMod, CastFunc, Msg);
+                        _ ->
+                            {error, srv_not_link}
+                    end;
+                _ ->
+                    {error, not_link}
+            end
+    end;
+cast({msg, {node, Node}, Name}, CastMod, CastFunc, Msg) when Node =:= node() ->
+    do_cast_msg(local, Name, CastMod, CastFunc, Msg);
+cast({msg, {node, Node}, Name}, CastMod, CastFunc, Msg) ->
+    case srv_lib:server_type() of
+        center ->
+            do_cast_msg(Node, Name, CastMod, CastFunc, Msg);
+        zone ->
+            cast(center, ?MODULE, cast, [{msg, {node, Node}, Name}, CastMod, CastFunc, Msg]) %% 交给中央服执行
     end;
 cast(Flag, _M, _F, _A) ->
     ?error("跨服异步调用类型错误：~w", [Flag]),
     {error, {bad_flag, Flag}}.
 
+do_cast(local, M, F, A) ->
+    catch util:apply(M, F, A),
+    true;
+do_cast(Node, M, F, A) ->
+    rpc:cast(Node, M, F, A).
+
+do_cast_msg(local, Name, CastMod, CastFunc, Msg) ->
+    catch CastMod:CastFunc(Name, Msg),
+    true;
+do_cast_msg(Node, Name, CastMod, CastFunc, Msg) ->
+    catch CastMod:CastFunc({Name, Node}, Msg),
+    true.
+
 %% @doc 同步执行方法
--spec call(center | center_ver | srv_id() | node(), module(), atom(), list()) -> true | {error, term()}.
+-spec call(center | {srv, srv_id()} | {node, node()}, module(), atom(), list()) -> term() | {error, term()} | {badrpc, term()}.
 %% 中央服节点执行，并且要与中央服节点版本一致
 call(center, M, F, A) ->
     case srv_lib:server_type() of
         center ->
-            util:apply(M, F, A);
+            do_call(local, M, F, A);
         zone ->
             case is_link() of
                 true ->
-                    rpc:call(srv_lib:center_node(), M, F, A, ?cluster_call_timeout);
-                _ ->
-                    {error, not_link}
-            end
-    end;
-%% 中央服节点执行
-call(center_ver, M, F, A) ->
-    case srv_lib:server_type() of
-        center ->
-            util:apply(M, F, A);
-        zone ->
-            case is_link_ver() of
-                true ->
-                    rpc:call(srv_lib:center_node(), M, F, A, ?cluster_call_timeout);
-                _ ->
-                    {error, not_link}
-            end
-    end;
-%% 指定游戏服节点执行，本游戏服节点和目标游戏服节点与中央服节点版本一致
-call({srv, SrvId}, M, F, A) ->
-    case srv_lib:is_local(SrvId) of
-        true ->
-            util:apply(M, F, A);
-        _ ->
-            case is_link() of
-                true ->
-                    case catch ets:lookup(srv_id, SrvId) of
-                        [{_, Node}] ->
-                            call(Node, M, F, A);
-                        _ ->
-                            {error, srv_not_link}
-                    end;
+                    do_call(srv_lib:center_node(), M, F, A);
                 _ ->
                     {error, not_link}
             end
     end;
 %% 指定游戏服节点执行
-call({srv_ver, SrvId}, M, F, A) ->
+call({srv, SrvId}, M, F, A) ->
     case srv_lib:is_local(SrvId) of
         true ->
-            util:apply(M, F, A);
+            do_call(local, M, F, A);
         _ ->
-            case is_link_ver() of
+            case is_link() of
                 true ->
-                    case catch ets:lookup(srv_id_ver, SrvId) of
+                    case catch ets:lookup(srv_id, SrvId) of
                         [{_, Node}] ->
-                            call(Node, M, F, A);
+                            call({node, Node}, M, F, A);
                         _ ->
                             {error, srv_not_link}
                     end;
@@ -191,18 +178,66 @@ call({srv_ver, SrvId}, M, F, A) ->
             end
     end;
 %% 游戏节点执行
-call(Node, M, F, A) when Node =:= node() ->
-    util:apply(M, F, A);
-call(Node, M, F, A) when is_atom(Node) ->
+call({node, Node}, M, F, A) when Node =:= node() ->
+    do_call(local, M, F, A);
+call({node, Node}, M, F, A) ->
     case srv_lib:server_type() of
         center ->
-            rpc:call(Node, M, F, A, ?cluster_call_timeout);
+            do_call(Node, M, F, A);
         zone ->
-            call(center, ?MODULE, call, [Node, M, F, A]) %% 交给中央服执行
+            call(center, ?MODULE, call, [{node, Node}, M, F, A]) %% 交给中央服执行
+    end;
+call({msg, center, Name}, CallMod, CallFunc, Msg) ->
+    case srv_lib:server_type() of
+        center ->
+            do_call_msg(local, Name, CallMod, CallFunc, Msg);
+        zone ->
+            case is_link() of
+                true ->
+                    do_call_msg(srv_lib:center_node(), Name, CallMod, CallFunc, Msg);
+                _ ->
+                    {error, not_link}
+            end
+    end;
+call({msg, {srv, SrvId}, Name}, CallMod, CallFunc, Msg) ->
+    case srv_lib:is_local(SrvId) of
+        true ->
+            do_call_msg(local, Name, CallMod, CallFunc, Msg);
+        _ ->
+            case is_link() of
+                true ->
+                    case catch ets:lookup(srv_id, SrvId) of
+                        [{_, Node}] ->
+                            call({msg, {node, Node}, Name}, CallMod, CallFunc, Msg);
+                        _ ->
+                            {error, srv_not_link}
+                    end;
+                _ ->
+                    {error, not_link}
+            end
+    end;
+call({msg, {node, Node}, Name}, CallMod, CallFunc, Msg) when Node =:= node() ->
+    do_call_msg(local, Name, CallMod, CallFunc, Msg);
+call({msg, {node, Node}, Name}, CallMod, CallFunc, Msg) ->
+    case srv_lib:server_type() of
+        center ->
+            do_call_msg(Node, Name, CallMod, CallFunc, Msg);
+        zone ->
+            call(center, ?MODULE, call, [{msg, {node, Node}, Name}, CallMod, CallFunc, Msg]) %% 交给中央服执行
     end;
 call(Flag, _M, _F, _A) ->
     ?error("跨服同步调用类型错误：~w", [Flag]),
     {error, {bad_flag, Flag}}.
+
+do_call(local, M, F, A) ->
+    util:apply(M, F, A);
+do_call(Node, M, F, A) ->
+    rpc:call(Node, M, F, A, ?cluster_call_timeout).
+
+do_call_msg(local, Name, CallMod, CallFunc, Msg) ->
+    CallMod:CallFunc(Name, Msg);
+do_call_msg(Node, Name, CallMod, CallFunc, Msg) ->
+    CallMod:CallFunc({Name, Node}, Msg).
 
 %% @doc 是否连接到中央服节点，并且与中央服节点版本一致
 -spec is_link() -> boolean().
@@ -217,11 +252,16 @@ is_link_ver() ->
 %% @doc 主动连接中央服节点
 -spec link() -> void().
 link() ->
-    case is_link_ver() of
-        true ->
+    case srv_lib:server_type() of
+        center ->
             ignore;
-        _ ->
-            cluster_mgr:info(check_connect)
+        zone ->
+            case is_link_ver() of
+                true ->
+                    ignore;
+                _ ->
+                    cluster_mgr:info(check_connect)
+            end
     end.
 
 %% @doc 连接到中央服节点触发
